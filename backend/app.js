5767d9bcd2e6d695e6eaa4477c690d988f74b990
JD Calvelli <jdcalvelli@gmail.com>
2022-06-22 13:37:10 -0500

    plaid integration complete with token passing to get access token

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,60 @@ const plaidClient = new plaid.PlaidApi(plaidConfig);
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
+// this is the homepage
 app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/index.html"))
+})
+
+// this is to create a link token
+app.post('/api/create-link-token', async (req, res) => {
+    const toPlaidRequest = {
+        user: {
+            client_user_id: 'test123',
+        },
+        client_name: "Mutual Aid Test",
+        products: ['auth'],
+        language: 'en',
+        country_codes: ['US']
+    }
+
+    try {
+        const createTokenResponse = await plaidClient.linkTokenCreate(toPlaidRequest);
+        res.json(createTokenResponse.data)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// this is to exchange the public token from link for the access token
+app.post('/api/exchange-public-token', express.urlencoded(), async (req, res)=> {
+    
+    console.log("-------")
+    console.log(req.body)
+    console.log("-------")
+    
+
+    const publicToken = req.body.public_token
+    console.log("-------")
+    console.log(publicToken)
+    console.log("-------")
+
+    try {
+        const fromPlaidResponse = await plaidClient.itemPublicTokenExchange({
+            public_token: publicToken
+        })
+        const accessToken = fromPlaidResponse.data.access_token
+
+        console.log("-------")
+        console.log(accessToken)
+        console.log("-------")
+
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 app.listen(PORT, () => {
