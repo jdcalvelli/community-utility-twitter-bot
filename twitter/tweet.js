abbbbfeffbdffaeb1152bc864d3cb5a0293b96cf
JD Calvelli <jdcalvelli@gmail.com>
2022-06-29 11:34:56 -0500

    adding a check to see if transaction data exists, yes tweet, no dont tweet

--- twitter/tweet.js
+++ twitter/tweet.js
@@ -15,29 +15,35 @@ async function getTransactionData(startDateString, endDateString) {
 
 (async function createTweetThread() {
     // start date is 2 days before current day
-    let startDate = `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1 <= 9 ? "0" + (new Date().getUTCMonth() + 1): new Date().getUTCMonth() + 1}-${new Date().getUTCDate() - 2 <=9 ? "0" + (new Date().getUTCDate() - 2) : new Date().getUTCDate() - 2}`
+    let startDate = `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1 <= 9 ? "0" + (new Date().getUTCMonth() + 1) : new Date().getUTCMonth() + 1}-${new Date().getUTCDate() - 2 <= 9 ? "0" + (new Date().getUTCDate() - 2) : new Date().getUTCDate() - 2}`
     // end date is 1 day before current day to capture transactions
-    let endDate = `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1 <= 9 ? "0" + (new Date().getUTCMonth() + 1): new Date().getUTCMonth() + 1}-${new Date().getUTCDate() - 1 <=9 ? "0" + (new Date().getUTCDate() - 1) : new Date().getUTCDate() - 1}`
+    let endDate = `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1 <= 9 ? "0" + (new Date().getUTCMonth() + 1) : new Date().getUTCMonth() + 1}-${new Date().getUTCDate() - 1 <= 9 ? "0" + (new Date().getUTCDate() - 1) : new Date().getUTCDate() - 1}`
 
     let pastDayTransactionData = await getTransactionData("2022-01-01", "2022-01-02")
 
     // create the tweet thread and then loop through the transaction data and add it to the tweet thread
 
-    let tweetThread = [`Here are CommunityUtility's transactions from ${endDate}`]
+    // check if transaction data length is greater than zero, if so then tweet if not do not tweet
 
-    for (let i = 0; i < pastDayTransactionData.transactions.length; i++) {
-        tweetThread.push({
-            text: `Authorized Date: ${pastDayTransactionData.transactions[i].authorized_date}\nPayment Method: ${pastDayTransactionData.transactions[i].payment_meta.payment_method}\nMerchant Name: ${pastDayTransactionData.transactions[i].merchant_name}\nPayment Amount: ${pastDayTransactionData.transactions[i].amount}`
-        })
+    if (pastDayTransactionData.transactions.length > 0) {
+        let tweetThread = [`Here are CommunityUtility's transactions from ${endDate}`]
+
+        for (let i = 0; i < pastDayTransactionData.transactions.length; i++) {
+            tweetThread.push({
+                text: `Authorized Date: ${pastDayTransactionData.transactions[i].authorized_date}\nPayment Method: ${pastDayTransactionData.transactions[i].payment_meta.payment_method}\nMerchant Name: ${pastDayTransactionData.transactions[i].merchant_name}\nPayment Amount: ${pastDayTransactionData.transactions[i].amount}`
+            })
+        }
+
+        tweetThread.push('Help out someone in need here: https://communityutility.org/')
+
+        try {
+            twitterConfig.twitterClient.v2.tweetThread(tweetThread)
+            console.log("DAILY TWEET FIRED SUCCESSFULLY")
+        } catch (error) {
+            console.log(error)
+        }
     }
-
-    tweetThread.push('Help out someone in need here: https://communityutility.org/')
-
-    try {
-        twitterConfig.twitterClient.v2.tweetThread(tweetThread)
-        console.log("DAILY TWEET FIRED SUCCESSFULLY")
-    } catch (error) {
-        console.log(error)
+    else {
+        console.log(`NO TRANSACTIONS ON ${startDate}`)
     }
-
 })()
