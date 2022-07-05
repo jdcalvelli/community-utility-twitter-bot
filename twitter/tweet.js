c8859ca0e91e9ee464bcafa88b4f5f2f75805d55
JD Calvelli <jdcalvelli@gmail.com>
2022-07-05 11:59:35 -0500

    fixing date strings so that plaid api gettransaction can read them

--- twitter/tweet.js
+++ twitter/tweet.js
@@ -22,12 +22,13 @@ async function getTransactionData(startDateString, endDateString) {
 }
 
 (async function createTweetThread() {
-    // start date is sunday at beginning of the week
-    let startDate = `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1 <= 9 ? "0" + (new Date().getUTCMonth() + 1) : new Date().getUTCMonth() + 1}-${new Date().getUTCDate() - 6 <= 9 ? "0" + (new Date().getUTCDate() - 6) : new Date().getUTCDate() - 6}`
-    // end date is day when cron job gets run - which is saturday at 11:55 pm
-    let endDate = `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1 <= 9 ? "0" + (new Date().getUTCMonth() + 1) : new Date().getUTCMonth() + 1}-${new Date().getUTCDate() <= 9 ? "0" + (new Date().getUTCDate()) : new Date().getUTCDate()}`
+    let endDate = new Date()
+    let startDate = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate() - 7)
 
-    let pastWeekTransactionData = await getTransactionData(startDate, endDate)
+    let endISO = endDate.toISOString().split('T')[0]
+    let startISO = startDate.toISOString().split('T')[0]
+
+    let pastWeekTransactionData = await getTransactionData(startISO, endISO)
 
     // create the tweet thread and then loop through the transaction data and add it to the tweet thread
 
